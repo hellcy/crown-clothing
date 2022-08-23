9467dada10afd908fd3e524654c86c53f560364e
Yuan Cheng <ycheng@atlassian.com>
2022-08-23 20:41:15 +1000

    Create sign up, form and button component

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,23 @@
+import "./button.styles.scss";
+
+/*
+  Button is just a styling wrapper around the original button component
+  So whatever children passed in to Button should also be passed into button
+*/
+const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
