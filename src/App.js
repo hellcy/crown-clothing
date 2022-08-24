5a19cbf13a5c918168a22e07effee31483055632
Yuan Cheng <ycheng@atlassian.com>
2022-08-24 21:37:36 +1000

    Add cart context and dropdown

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return <div>I am the Shop page</div>;
-};
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
